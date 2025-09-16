fae6f0efb28e13fa20b0056d39e55c73409a2113
Raydzan <mochraydzan4420@gmail.com>
2025-09-17 00:42:12 +0700

    new save T3

--- main/models.py
+++ main/models.py
@@ -1,32 +1,21 @@
-# main/models.py
 import uuid
 from django.db import models
 
-class News(models.Model):
+class Product(models.Model):
     CATEGORY_CHOICES = [
-        ('transfer', 'Transfer'),
-        ('update', 'Update'),
-        ('exclusive', 'Exclusive'),
-        ('match', 'Match'),
-        ('rumor', 'Rumor'),
-        ('analysis', 'Analysis'),
+        ('jersey', 'Jersey'),
+        ('shoes', 'Sepatu Bola'),
+        ('accessory', 'Aksesoris'),
+        ('ball', 'Bola'),
+        ('others', 'Lainnya'),
     ]
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
-    news_views = models.PositiveIntegerField(default=0)
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='jersey')
+    image_url = models.URLField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    is_featured = models.BooleanField(default=False)
-    price = models.IntegerField(default=0) 
+
     def __str__(self):
-        return self.title
-
-    @property
-    def is_news_hot(self):
-        return self.news_views > 20
-
-    def increment_views(self):
-        self.news_views += 1
-        self.save(update_fields=['news_views'])
+        return self.name
--- main/views.py
+++ main/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.core import serializers
-from .models import News
 
 def show_main(request):
     context = {
