978e93e4992be4da701208a86e1089ea3cb71dc1
Raydzan <mochraydzan4420@gmail.com>
2025-10-15 22:20:45 +0700

    perbaikan kecil

--- IniFootballShop/settings.py
+++ IniFootballShop/settings.py
@@ -30,7 +30,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "moch.raydzan-inifootballshop.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "moch-raydzan-inifootballshop.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
     "https://moch-raydzan-inifootballshop.pbp.cs.ui.ac.id"
 ]
